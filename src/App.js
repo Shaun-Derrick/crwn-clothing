385c2999a0b9e1e58eac6f6969daa5ae6078c776
Shaun-Derrick <derrickshaun@gmail.com>
2021-03-08 14:15:22 -0700

    Added header component and links to other pages

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import { Switch, Route } from "react-router-dom"
 import "./App.css"
 import HomePage from "./pages/homepage/homepage.component"
 import ShopPage from "./pages/shop/shop.component"
+import Header from "./components/header/header.component"
 
 function App() {
   return (
     <div>
+      {/* header exists outside of the switch so it is always rendered regardless of what page is actively being rendered */}
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
