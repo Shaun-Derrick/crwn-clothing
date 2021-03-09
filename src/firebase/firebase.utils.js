694b0c94ebc3cb6186042c17aeffdc422314f0a8
Shaun-Derrick <derrickshaun@gmail.com>
2021-03-08 21:54:59 -0700

    Added firebase and google sign in

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,25 @@
+import firebase from "firebase/app"
+import "firebase/firestore"
+import "firebase/auth"
+import { initialize } from "workbox-google-analytics"
+
+const config = {
+  apiKey: "AIzaSyBTPL03rdhzaw9w-abyyUHdw7gOQtjzTJM",
+  authDomain: "crwn-db-7af58.firebaseapp.com",
+  projectId: "crwn-db-7af58",
+  storageBucket: "crwn-db-7af58.appspot.com",
+  messagingSenderId: "173954184683",
+  appId: "1:173954184683:web:9f22ed1a08250f652755ae"
+}
+
+firebase.initializeApp(config)
+
+export const auth = firebase.auth()
+export const firestore = firebase.firestore()
+
+const provider = new firebase.auth.GoogleAuthProvider()
+provider.setCustomParameters({ prompt: "select_account" })
+
+export const signInWithGoogle = () => auth.signInWithPopup(provider)
+
+export default firebase
