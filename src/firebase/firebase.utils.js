53da90c109763062a8f08d29b37b92cedf767b69
Shaun-Derrick <derrickshaun@gmail.com>
2021-03-09 11:52:35 -0700

    Created sign in page with integrated component and sign in with google

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,7 +1,6 @@
 import firebase from "firebase/app"
 import "firebase/firestore"
 import "firebase/auth"
-import { initialize } from "workbox-google-analytics"
 
 const config = {
   apiKey: "AIzaSyBTPL03rdhzaw9w-abyyUHdw7gOQtjzTJM",
