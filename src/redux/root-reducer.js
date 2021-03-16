4577780c2dc87781747eabdc5654a697d11f9226
Shaun-Derrick <derrickshaun@gmail.com>
2021-03-16 12:14:13 -0600

    Refactored shop component and added collectionsOverview component

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,25 @@
 import { combineReducers } from "redux"
+import { persistReducer } from "redux-persist"
+import storage from "redux-persist/lib/storage"
 
 import userReducer from "./user/user.reducer"
 import cartReducer from "./cart/cart.reducer"
+import directoryReducer from "./directory/directory.reducer"
+import shopReducer from "./shop/shop.reducer"
 
-export default combineReducers({
+const persistConfig = {
+  key: "root",
+  storage,
+  // Pass in the reducer you want to persist into the whitelist array
+  whitelist: ["cart"]
+}
+
+const rootReducer = combineReducers({
+  // you do not have to persist user because firebase automatically does so
   user: userReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  directory: directoryReducer,
+  shop: shopReducer
 })
+
+export default persistReducer(persistConfig, rootReducer)
