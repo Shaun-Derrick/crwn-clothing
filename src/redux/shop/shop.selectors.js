8679ab02ab90e9b269fc1f37cf99222073e5126b
Shaun-Derrick <derrickshaun@gmail.com>
2021-03-16 17:37:02 -0600

    Moved out directory and shop data into its own reducer and refactored redux flow

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,8 +1,38 @@
 import { createSelector } from "reselect"
+import memoize from "lodash.memoize"
+
+// refactored shop_data array to an object to eliminate the need to use find to return the user selected collection-data normalization
+// const COLLECTION_ID_MAP = {
+//   hats: 1,
+//   sneakers: 2,
+//   jackets: 3,
+//   womens: 4,
+//   mens: 5
+// }
 
 const selectShop = (state) => state.shop
 
 export const selectCollections = createSelector(
   [selectShop],
   (shop) => shop.collections
+)
+
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  // converts a object into an array
+  (collections) => Object.keys(collections).map((key) => collections[key])
+)
+
+// export const selectCollection = (collectionUrlParam) =>
+//   createSelector([selectCollections], (collections) =>
+//     collections.find(
+//       (collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
+//     )
+//   )
+// selector changed to memoize url parameter -- COLLECTION_ID_MAP no longer needed
+export const selectCollection = memoize((collectionUrlParam) =>
+  createSelector(
+    [selectCollections],
+    (collections) => collections[collectionUrlParam]
+  )
 )
